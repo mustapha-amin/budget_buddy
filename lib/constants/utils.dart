eec6deb0cbf93f697180d8c46193c921368a648e
mustapha amin <mustaphaamin2003@gmail.com>
2023-05-16 11:31:26 -0700

    Edited utils class

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 class Utils {
   BuildContext? context;
   Utils(this.context);
-  Size get screenSize => MediaQuery.of(context!).size;
+  
+  double get screenHeight => MediaQuery.of(context).size.height;
+  double get screenWidth => MediaQuery.of(context!).size.width;
 }
