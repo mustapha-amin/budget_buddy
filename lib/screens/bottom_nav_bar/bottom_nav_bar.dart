4796197f1cd2e9c8f713d776dbe4cb772b8126e0
mustapha amin <mustaphaamin2003@gmail.com>
2023-05-16 07:49:43 -0700

    created the utils class

---
+++ lib/screens/bottom_nav_bar/bottom_nav_bar.dart
@@ -0,0 +1,48 @@
+import 'package:budget_buddy/screens/bottom_nav_bar_items/accounts.dart';
+import 'package:budget_buddy/screens/bottom_nav_bar_items/home.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  final List pages = const [
+    Home(),
+    Accounts(),
+  ];
+
+  void selectPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: selectPage,
+        currentIndex: _selectedIndex,
+        items: const  [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: "Accounts",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/bottom_nav_bar_items/accounts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Accounts extends StatefulWidget {
+  const Accounts({super.key});
+
+  @override
+  State<Accounts> createState() => _AccountsState();
+}
+
+class _AccountsState extends State<Accounts> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
